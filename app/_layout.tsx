6980a66acc856159db8ece98027c95104fdcf9cc
Rida-Lamiini <ridalamiini0@gmail.com>
2025-03-17 11:33:57 +0000

    update expo-router to version 4.0.19 and add custom font styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "Outfit-Regular": require("../assets/fonts/Outfit-Regular.ttf"),
+    "Outfit-ExtraBold": require("../assets/fonts/Outfit-ExtraBold.ttf"),
+  });
   return <Stack />;
 }
