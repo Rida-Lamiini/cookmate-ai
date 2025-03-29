d12b486b52cfc7de46801244788b8daef47f5b9a
Rida-Lamiini <ridalamiini0@gmail.com>
2025-03-29 23:21:42 +0000

    enhance authentication flow with animated sign-in screen; add username input for sign-up and implement sign-out functionality in profile

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,10 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { useClerk } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
 
 export default function Profile() {
+  const { signOut } = useClerk();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      // Redirect to your desired page
+      Linking.openURL(Linking.createURL("/Landing"));
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <TouchableOpacity onPress={handleSignOut}>
+      <Text>Sign out</Text>
+    </TouchableOpacity>
   );
 }
